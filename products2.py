ab2dd2dfb913cf1d2c9f6a4180976257fa671354
Daniel Hung <see98524@gmail.com>
2019-09-15 09:49:48 +0800

    add header

--- products2.py
+++ products2.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
+	price = int(price)
 	p = []
 	p.append(name)
 	p.append(price) #上三行可直接寫成p = [name, price]
@@ -17,6 +18,8 @@ for p in products:
 
 # 'abc' + '123' = 'abc123' 字串可+
 
-with open ('products.csv', 'w') as f: #當資料中有多種不同屬性之變數，可用exl打開
+with open ('products2.csv', 'w', encoding = 'utf-8') as f: #當資料中有多種不同屬性之變數，可用exl打開
+	f.write('商品,價格\n')
 	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n') # +逗號是為了讓不同屬性可隔成兩格
+		f.write(p[0] + ',' + str(p[1]) + '\n') # +逗號是為了讓不同屬性可隔成兩格
+#使用exl打開檔案仍會有亂碼產生的情況，這是因為exl讀取模式並未使用UTF-8編碼
